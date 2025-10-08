5af4bf345c5fb72e4e38d16fa39d3e9fc2992939
Jan Kropiwnicki <jan.kropiwnicki@infotech.edu.pl>
2025-10-08 08:53:00 +0200

    Sample random questions

--- app/src/main/java/com/example/kotlin_wstep/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_wstep/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.kotlin_wstep
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val questionView = findViewById<TextView>(R.id.question)
+        questionView.text = when ((0..2).random()) {
+            0 -> "Jak długą trzeba mieć brodę żeby zostać sysadminem?"
+            1 -> "Jak napisać pętlę for w HTMLu?"
+            2 -> "Dlaczego JavaScript nas zachwyca?"
+            else -> "Nie znaleziono pytania"
         }
     }
 }
---
+++ app/src/main/java/com/example/kotlin_wstep/QuestionFragment.kt
@@ -0,0 +1,4 @@
+package com.example.kotlin_wstep
+
+class QuestionFragment {
+}
