3cc96930d94bce420864fd06dd06ff57a4be41c8
Jan Kropiwnicki <jan.kropiwnicki@infotech.edu.pl>
2025-10-09 14:40:41 +0200

    Separated question and main activity

--- app/src/main/java/com/example/kotlin_wstep/QuestionActivity.kt
+++ app/src/main/java/com/example/kotlin_wstep/QuestionActivity.kt
@@ -1,14 +1,13 @@
 package com.example.kotlin_wstep
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class QuestionActivity : AppCompatActivity(R.layout.activity_question) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,10 +18,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             insets
         }
 
-        findViewById<Button>(R.id.startQuestionsButton)
-            .setOnClickListener {
-                val intent = Intent(this, QuestionActivity::class.java)
-                startActivity(intent)
-            }
+        val questionView = findViewById<TextView>(R.id.questionView)
+        questionView.text = when ((0..2).random()) {
+            0 -> "Jak długą trzeba mieć brodę żeby zostać sysadminem?"
+            1 -> "Jak napisać pętlę for w HTMLu?"
+            2 -> "Dlaczego JavaScript nas zachwyca?"
+            else -> "Nie znaleziono pytania"
+        }
     }
 }
--- app/src/main/java/com/example/kotlin_wstep/QuestionFragment.kt
+++
@@ -1,4 +0,0 @@
-package com.example.kotlin_wstep
-
-class QuestionFragment {
-}
